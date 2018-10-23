6cf7551392c1295f1a4178aa0cb07af35c31cfa7
Thomas Lynn <thomaslynn01@gmail.com>
2018-10-23 23:08:26 +0100

    Moved more code away from main

--- src/kanji/RipperMain.java
+++ src/kanji/RipperMain.java
@@ -2,7 +2,6 @@ package kanji;
 
 import java.awt.Dimension;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,24 +16,26 @@ public class RipperMain {
 	 */
 	// amount of threads to run the program on
 	private static int poolSize = Runtime.getRuntime().availableProcessors();
-	
-	// stems of info used. This determines what txt file to read and what folder to output to
-	//public static String[] stems = new String[] { "n5", "n4", "n3", "n2" }; // uncomment this to run a full-scale run
+
+	// stems of info used. This determines what txt file to read and what folder to
+	// output to
+	// public static String[] stems = new String[] { "n5", "n4", "n3", "n2" }; //
+	// uncomment this to run a full-scale run
 	public static String[] stems = new String[] { "ntest" }; // uncomment this to do tests
-	
+
 	// lists all the image resolutions to output as
 	public static Dimension[] dimensions = new Dimension[] { new Dimension(1280, 720), new Dimension(1920, 1080),
 			new Dimension(2560, 1440), new Dimension(3840, 2160) };
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("Thread count: "+poolSize);
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		System.out.println("Thread count: " + poolSize);
 		ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
+		
 		long ltimer = System.currentTimeMillis();
 		for (String stem : stems) {
-			String inputFileName = stem + ".txt";
 			ArrayList<String> urls = new ArrayList<String>();
-			FileReader fileReader = new FileReader(inputFileName);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(stem + ".txt"));
 			String line = null;
 			while ((line = bufferedReader.readLine()) != null) {
 				urls.add(line);
@@ -43,45 +44,13 @@ public class RipperMain {
 
 			for (String url : urls) {
 				if (url.length() > 0) {
-					System.out.println("processing: " + url);
-					executorService.execute(new Runnable() {
-						public void run() {
-							try {
-								long timer = System.currentTimeMillis();
-								
-								// rip data from the webpage
-								PageData pageData = (new DataRipper(url)).getData();
-								
-								long datatime = System.currentTimeMillis() - timer;
-								long imagetimer = System.currentTimeMillis();
-								
-								for (Dimension d : dimensions) {
-									String outputFolder = "output/" + d.width + "x" + d.height + "/" + stem + "images";
-									new File(outputFolder).mkdirs();
-									ImageMaker.makeImage(pageData, d.width, d.height, outputFolder);
-								}
-								
-								long time = System.currentTimeMillis();
-								System.out.println("data: " + datatime + " image: " + (time - imagetimer) + " full: "
-										+ (time - timer) + "ms");
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						}
-					});
+					executorService.execute(new Worker(stem, url));
 				}
 			}
 		}
-		try {
-			executorService.shutdown();
-			executorService.awaitTermination(1, TimeUnit.HOURS);
-			System.out.println("---------full time: " + (System.currentTimeMillis() - ltimer) + "ms");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		executorService.shutdown();
+		executorService.awaitTermination(1, TimeUnit.HOURS);
+		System.out.println("full execution time: " + (System.currentTimeMillis() - ltimer) + "ms");
 	}
 
-	
-
-	
 }
