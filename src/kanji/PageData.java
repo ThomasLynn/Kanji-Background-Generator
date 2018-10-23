617a7a5b21c2fbf838125b808f82e98e2caaecd0
Thomas Lynn <thomaslynn01@gmail.com>
2018-10-23 18:49:26 +0100

    pushing all the useful stuff

---
+++ src/kanji/PageData.java
@@ -0,0 +1,86 @@
+package kanji;
+
+import java.util.ArrayList;
+
+public class PageData{
+	public String character;
+	public String meaning;
+	public ArrayList<String> onyomi;
+	public ArrayList<String> kunyomi;
+
+	public PageData(String character, String meaning, ArrayList<String> onyomi, ArrayList<String> kunyomi,
+			int onyomiCutSize, int kunyomiCutSize) {
+		this.character = character;
+		String[] meaningList = meaning.split(", ");
+		String newMeaning = "";
+		for (int j = 0; true; j++) {
+			String tempNewMeaning = newMeaning + meaningList[j];
+			if (tempNewMeaning.length() > 40) {
+				break;
+			}
+			if (j >= meaningList.length - 1) {
+				newMeaning = tempNewMeaning;
+				break;
+			}
+			newMeaning = tempNewMeaning + ", ";
+		}
+		if (newMeaning.charAt(newMeaning.length() - 2) == ',') {
+			newMeaning = newMeaning.substring(0, newMeaning.length() - 2);
+		}
+		this.meaning = newMeaning;
+		this.onyomi = onyomi;
+		this.kunyomi = kunyomi;
+		processDef(onyomi, onyomiCutSize);
+		processDef(kunyomi, onyomiCutSize);
+	}
+
+	public void processDef(ArrayList<String> yomi, int cutSize) {
+		for (int i = 0; i < yomi.size(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (yomi.get(i).equals(yomi.get(j))) {
+					yomi.remove(i);
+					i--;
+				}
+			}
+		}
+		while (yomi.size() > cutSize) {
+			yomi.remove(cutSize);
+		}
+		for (int i = 0; i < yomi.size(); i++) {
+			String nStr = yomi.get(i);
+
+			String info = nStr.substring(nStr.indexOf("】") + 2);
+
+			ArrayList<String> def = new ArrayList<String>();
+
+			for (String s : info.split(", (?![^(]*\\))")) {
+				def.add(s);
+			}
+			def.sort((x, y) -> Integer.compare(x.length(), y.length()));
+			String newInfo = "";
+			for (int j = 0; true; j++) {
+				String tempNewInfo = newInfo + def.get(j);
+				if (tempNewInfo.length() > 80) {
+					break;
+				}
+				if (j >= def.size() - 1) {
+					newInfo = tempNewInfo;
+					break;
+				}
+				newInfo = tempNewInfo + ", ";
+			}
+			if (newInfo.length() > 0 && nStr.contains(character)) {
+				yomi.set(i, JapaneseCharacter.convertToKatakana(nStr.substring(0, nStr.indexOf("】") + 1)));
+				i++;
+				if (newInfo.charAt(newInfo.length() - 2) == ',') {
+					newInfo = newInfo.substring(0, newInfo.length() - 2);
+				}
+				yomi.add(i, newInfo);
+
+			} else {
+				yomi.remove(i);
+				i--;
+			}
+		}
+	}
+}
