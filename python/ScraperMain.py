966393b0f12e6bb68753750b5099af2583d4570e
Thomas Lynn <thomaslynn01@gmail.com>
2021-04-09 19:38:19 +0100

    renamed list files

--- python/ScraperMain.py
+++ python/ScraperMain.py
@@ -4,12 +4,22 @@ from mezmorize import Cache
 
 cache = Cache(CACHE_TYPE='filesystem', CACHE_DIR='cache')
     
-@cache.memoize()
-def fibonacci(n):
-    print("fib",n)
-    if n < 2:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
+#@cache.memoize()
+def get_jisho_data(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='kanji-details__main-meanings')
+    print("results",results)
+    return None
     
     
-print("fib 100",fibonacci(100))
+def scrape_list(jisho_list_filename):
+    with open(jisho_list_filename) as f:
+        urls = f.readlines()
+        urls = [x.strip() for x in urls] 
+        urls = [value for value in urls if value != ""]
+    
+    for w in urls:
+        get_jisho_data(w)
+    
+scrape_list("lists/jisho_small.txt")
