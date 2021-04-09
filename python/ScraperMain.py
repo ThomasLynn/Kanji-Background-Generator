f283af3966f1dfbb1f0ff13c3b376f3c9475099d
Thomas Lynn <thomaslynn01@gmail.com>
2021-04-10 00:12:24 +0100

    added tatoeba example sentences in english and japanese

--- python/ScraperMain.py
+++ python/ScraperMain.py
@@ -1,19 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 from mezmorize import Cache
+import urllib.parse
+import re
 
 cache = Cache(CACHE_TYPE='filesystem', CACHE_DIR='cache', CACHE_DEFAULT_TIMEOUT = 1e10)
     
 @cache.memoize()
-def get_jisho_data(url):
+def get_web_data(url):
     page = requests.get(url)
-    print("encoding",page.encoding,page.apparent_encoding)
     return str(page.text)
     
 def scrape_jisho_data(url):
-    print("getting url",url)
-    print("types",type(url),type(get_jisho_data(url)))
-    soup = BeautifulSoup(get_jisho_data(url), 'html.parser')
+    print("getting jisho url",url)
+    soup = BeautifulSoup(get_web_data(url), 'html.parser')
     
     kanji = soup.find(class_='character')
     kanji = kanji.get_text()
@@ -22,7 +22,30 @@ def scrape_jisho_data(url):
     meaning = soup.find(class_='kanji-details__main-meanings')
     meaning = meaning.get_text()
     meaning = meaning.strip()
-    print("meaning",meaning,".")
+    print("meaning",meaning)
+    
+    sentences = scrape_tatoeba_data(
+        "https://tatoeba.org/eng/sentences/search?from=jpn&query="+urllib.parse.quote(kanji)+"&to=eng"
+    )
+    
+def scrape_tatoeba_data(url):
+    print("getting tatoeba url",url)
+    soup = BeautifulSoup(get_web_data(url), 'html.parser')
+    
+    sections = soup.find_all("div",class_="sentence-and-translations md-whiteframe-1dp")
+    print("found",len(sections),"sections")
+    for w in sections:
+        string = re.sub(r'\\u([0-9a-fA-F]{4})',lambda m: chr(int(m.group(1),16)),w["ng-init"])
+        japanese = re.search(r',"text":"(.*?)","lang":"jpn"', string).group(1)
+        print("japanese",japanese)
+        
+        # this is a really hacky way of doing this,
+        # if anyone actually knows regex, please change this
+        english = re.findall(',"gne":"gnal","(.*?)":"txet"', string[::-1])
+        for i in range(len(english)):
+            english[i] = english[i][::-1]
+        print("english", english)
+    return sections
     
     
 def scrape_list(jisho_list_filename):
@@ -34,4 +57,4 @@ def scrape_list(jisho_list_filename):
     for w in urls:
         scrape_jisho_data(w)
     
-scrape_list("lists/jisho_small.txt")
+scrape_list("lists/jisho_single.txt")
