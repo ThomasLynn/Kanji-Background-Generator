45acd41ab3ab0d6ced217dce6884c5e935b1af92
Thomas Lynn <thomaslynn01@gmail.com>
2021-04-09 20:27:14 +0100

    caches page + scrapes kanji meaning

--- python/ScraperMain.py
+++ python/ScraperMain.py
@@ -4,14 +4,19 @@ from mezmorize import Cache
 
 cache = Cache(CACHE_TYPE='filesystem', CACHE_DIR='cache')
     
-#@cache.memoize()
+@cache.memoize()
 def get_jisho_data(url):
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(id='kanji-details__main-meanings')
-    print("results",results)
-    return None
+    return str(page.content)
     
+def scrape_jisho_data(url):
+    print("getting url",url)
+    print("types",type(url),type(get_jisho_data(url)))
+    soup = BeautifulSoup(get_jisho_data(url), 'html.parser')
+    results = soup.find(class_='kanji-details__main-meanings')
+    text = results.get_text()
+    text = text[2:].strip()[:-2]
+    print("results",text)
     
 def scrape_list(jisho_list_filename):
     with open(jisho_list_filename) as f:
@@ -20,6 +25,6 @@ def scrape_list(jisho_list_filename):
         urls = [value for value in urls if value != ""]
     
     for w in urls:
-        get_jisho_data(w)
+        scrape_jisho_data(w)
     
 scrape_list("lists/jisho_small.txt")
