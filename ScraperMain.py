082048576fec84d7c86c015b397a5ba4b120498e
Thomas Lynn <thomaslynn01@gmail.com>
2021-04-09 18:07:02 +0100

    switched to mezmorize for caching

--- ScraperMain.py
+++ ScraperMain.py
@@ -1,20 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-from functools import wraps
-import pickle
+from mezmorize import Cache
 
-def cached(func):
-    func.cache = {}
-    @wraps(func)
-    def wrapper(*args):
-        try:
-            return func.cache[args]
-        except KeyError:
-            func.cache[args] = result = func(*args)
-            return result   
-    return wrapper
+cache = Cache(CACHE_TYPE='filesystem', CACHE_DIR='cache')
     
-@cached
+@cache.memoize()
 def fibonacci(n):
     print("fib",n)
     if n < 2:
@@ -22,14 +12,4 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
     
     
-try:
-    with open('fibs.pickle', 'rb') as file:
-        fibonacci.cache = pickle.load(file)
-except:
-    print("unable to load cache")
-    
-print("cache",fibonacci.cache)
 print("fib 100",fibonacci(100))
-
-with open('fibs.pickle', 'wb') as file:
-    pickle.dump(fibonacci.cache, file, protocol=pickle.HIGHEST_PROTOCOL)
